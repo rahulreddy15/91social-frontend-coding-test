ff84aca6d000b2e1b8486fa3fcc6c3652aff2ddb
rahulreddy15 <arahulreddy15@gmail.com>
2020-08-24 11:17:31 +0530

    Added Pace To Indicate Loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Error404 from "./404";
 
-import Home from "./Home";
-import LaunchesHooks from "./launches/LaunchesHooks";
-import Rockets from "./rockets/Rockets";
-import Dragons from "./dragons/Dragons";
+const Home = lazy(() => import("./Home"));
+const LaunchesHooks = lazy(() => import("./launches/LaunchesHooks"));
+const Rockets = lazy(() => import("./rockets/Rockets"));
+const Dragons = lazy(() => import("./dragons/Dragons"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <React.Fragment>
+      <React.Fragment className="App Fade">
         <Header />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/launches" component={LaunchesHooks} />
-          <Route path="/rockets" component={Rockets} />
-          <Route path="/dragons" component={Dragons} />
-          <Route component={Error404} />
-        </Switch>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/launches" component={LaunchesHooks} />
+            <Route path="/rockets" component={Rockets} />
+            <Route path="/dragons" component={Dragons} />
+            <Route component={Error404} />
+          </Switch>
+        </Suspense>
         <Footer />
       </React.Fragment>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './reset.css';
-import './ui.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./reset.css";
+import "./ui.css";
+import "pace-js";
+import "pace-js/themes/blue/pace-theme-minimal.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
